ef2cfe30bad06cab9ba0eb7373bbac9d37de9838
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2023-12-05 17:31:58 -0600

    If

---
+++ Javascript/Control de flujos/if.js
@@ -0,0 +1,20 @@
+// Control de flujo
+
+if (true) {
+    console.log("estoy dentro de un if")    
+}
+
+if (false) {
+    console.log("no estoy dentro de un if")    
+}
+
+const edad = 5 //10
+
+if (edad > 5 && edad < 18) {
+    console.log("El niño puede jugar")
+}
+else {
+    console.log("No puedes jugar")
+}
+
+console.log("fin programa")
