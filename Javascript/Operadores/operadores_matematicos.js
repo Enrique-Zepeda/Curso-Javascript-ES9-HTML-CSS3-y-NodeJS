1a378bdbd325d4c769d45576a1ac7a976bc5cec6
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2023-12-04 20:17:03 -0600

    Operadores Matematicos

---
+++ Javascript/Operadores/operadores_matematicos.js
@@ -0,0 +1,37 @@
+//Operaciones basicas
+const suma = 1 + 2
+const resta = 1 - 2
+const multiplicacion = 2 * 3
+const division = 9 / 3
+console.log(suma, resta, multiplicacion, division);
+
+// el modulo sirve para mostrar el cociente de una division
+const modulo = 10 % 3
+console.log(modulo);
+
+// incrementar un valor es ++ y no se puede cambiar el valor de una constante
+// const num = 5
+let num = 5
+num++
+num++
+num++
+num++
+num++
+console.log(num);
+
+// decrementar
+let num2 = 10
+num2--
+num2--
+num2--
+num2--
+num2--
+console.log(num2);
+
+// operador de asignacion
+num += 5
+num -= 5
+num *= 5
+num /= 2
+console.log(num);
+
