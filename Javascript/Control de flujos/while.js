8e899de940d4c9ac420c900189a2f6118f8de165
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2023-12-05 17:45:34 -0600

    While

---
+++ Javascript/Control de flujos/while.js
@@ -0,0 +1,17 @@
+// control de flujo while
+
+// const x = 0 Nota no puedo cambiar un valor const
+let x = 0
+
+while (x < 5) {
+    console.log(x);
+    x++
+}
+
+let y = 0
+while (y <= 5) {
+    console.log(y);
+    y++
+}
+
+console.log('Terminando el loop')
