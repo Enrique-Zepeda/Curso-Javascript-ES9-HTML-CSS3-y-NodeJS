519a219f4fc2fa107ef6dee55cc596ea61e9a201
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2023-12-05 16:30:59 -0600

    Operadores de comparacion

---
+++ Javascript/Operadores/ope_compracion.js
@@ -0,0 +1,42 @@
+// operadores de comparacion
+// igualdad estricta
+const resultado1 = 5 === 6
+// igualdad no estricta, string pueden ser iguales a numeros si el valor es el mismo
+const resultado2 = 5 == 5
+const resultado3 = 5 == '5'
+// 5 es menor a 6
+const resultado4 = 5 < 6
+// 5 es menor que 4
+const resultado5 = 5 < 4
+// 5 es mayor que 4
+const resultado6 = 5 > 4
+// 5 es mayor que 8
+const resultado7 = 5 > 8
+// 5 es menor o igual que 5
+const resultado8 = 5 <= 5
+// 5 es menor o igual que 6
+const resultado9 = 5 <= 6
+// 5 es mayor o igual que 6
+const resultado10 = 5 >= 5
+// 5 es mayor o igual que 6
+const resultado11 = 5 >= 6
+
+// desigualdad
+// estrictamente desiguales
+const resultado12 = 5 !== '5' //resultado12 = 5 !== 5 este es falso porque 5 no es diferente que 5 y el otro es verdad porque 5 es diferente de '5' por el tipo de variable
+//  no estrictamente desiguales
+const resultado13 = 5 != '5'
+
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+console.log(resultado4);
+console.log(resultado5);
+console.log(resultado6);
+console.log(resultado7);
+console.log(resultado8);
+console.log(resultado9);
+console.log(resultado10);
+console.log(resultado11);
+console.log(resultado12);
+console.log(resultado13);
