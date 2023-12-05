3b8f1e2aaa13d1f54387394e38376f72d5cd958f
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2023-12-05 17:17:45 -0600

    Operadores Logicos

---
+++ Javascript/Operadores/ope_logicos.js
@@ -0,0 +1,20 @@
+// operadores logicos
+// or and not
+// || &&  !
+
+// operador OR (||) generalmente se usa con dos valores
+// const resultadoOR = false || true
+const resultadoOR = false || false || 'Hola' || 'mundo'
+console.log(resultadoOR)
+
+// operador and (&&) 
+const resultadoAnd = true && false
+console.log(resultadoAnd)
+
+// operador not (!)
+const resultadoNot = !true
+const resultadoNot2 = !false
+console.log(resultadoNot)
+console.log(resultadoNot2)
+
+// Los operadores de not y de and evaluan hasta que se encuentre con el primer valor que corte la operacion
