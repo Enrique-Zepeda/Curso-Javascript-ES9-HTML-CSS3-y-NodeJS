33b6f8c126a46999e38fe540e60994211da3fa5d
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2023-12-05 18:02:45 -0600

    switch

---
+++ Javascript/Control de flujos/whitch.js
@@ -0,0 +1,65 @@
+// control de flujo switch
+
+// caso 1
+switch (1) {
+    case 1: {
+        console.log("yo soy el caso 1")
+        break;
+    }
+    case 2:
+        console.log("yo soy el caso 2")
+        break;
+    case 3:
+        console.log("yo soy el caso 3")
+        break
+    default:
+        console.log("yo soy el caso default")
+}
+
+// caso 2
+switch (2) {
+    case 1: {
+        console.log("yo soy el caso 1")
+        break;
+    }
+    case 2:
+        console.log("yo soy el caso 2")
+        break;
+    case 3:
+        console.log("yo soy el caso 3")
+        break
+    default:
+        console.log("yo soy el caso default")
+}
+
+// caso 3
+switch (3) {
+    case 1: {
+        console.log("yo soy el caso 1")
+        break;
+    }
+    case 2:
+        console.log("yo soy el caso 2")
+        break;
+    case 3:
+        console.log("yo soy el caso 3")
+        break
+    default:
+        console.log("yo soy el caso default")
+}
+
+// caso default
+switch (6) {
+    case 1: {
+        console.log("yo soy el caso 1")
+        break;
+    }
+    case 2:
+        console.log("yo soy el caso 2")
+        break;
+    case 3:
+        console.log("yo soy el caso 3")
+        break
+    default:
+        console.log("yo soy el caso default")
+}
