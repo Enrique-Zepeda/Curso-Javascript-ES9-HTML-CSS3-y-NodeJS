da7cb6b3e0ae985ca1357df80f4accd5abd6a74a
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2023-12-10 09:55:51 -0600

    Manipular texto de forma dinamica

--- Javascript/DOM/main.js
+++ Javascript/DOM/main.js
@@ -1,4 +1,6 @@
 window.onload = () => {
 const parrafo = document.getElementById('text')
-console.log(parrafo.innerText)
+// console.log(parrafo.innerText)
+// con esto podemos cambiar el contenido de nuestra pagina de forma dinamica pero no se cambia en el html
+parrafo.innerText = 'Texto actualizado'
 }
