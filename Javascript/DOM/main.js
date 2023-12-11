3334bf4d423b039bc5da0dcee40de7d432ba4e22
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2023-12-11 11:59:50 -0600

    Agregar html

--- Javascript/DOM/main.js
+++ Javascript/DOM/main.js
@@ -3,4 +3,6 @@ const parrafo = document.getElementById('text')
 // console.log(parrafo.innerText)
 // con esto podemos cambiar el contenido de nuestra pagina de forma dinamica pero no se cambia en el html
 parrafo.innerText = 'Texto actualizado'
+parrafo.innerHTML= '<li>elemento 1</li><li>elemento 2</li>'
+
 }
