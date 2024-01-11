86ef9931196d432d50fed3453e97e6b78c1052a9
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-01-11 08:35:39 -0600

    Listando usuarios

---
+++ API rest/app/main.js
@@ -0,0 +1,58 @@
+const loadInitialTemplate = () => {
+    const template = `
+    <h1>Usuarios</h1>
+    <form id="user-form">
+        <div>
+            <label>Nombre</label>
+            <input name="name" />
+        </div>
+        <div>
+            <label>Apellido</label>
+            <input name="lastName" />
+        </div>
+        <button type="submit">Enviar</button>
+    </form>
+    <ul id="user-list"></ul>
+    `
+    const body = document.getElementsByTagName('body')[0]
+    body.innerHTML = template
+}
+
+const getUsers = async () => {
+    const response = await fetch('/users')
+    const users = await response.json()
+    console.log(users)
+    const template = user => `
+    <li>
+        ${user.name} ${user.lastName} <button data-id="${user._id}">Eliminar</button>
+    </li>
+    `
+    const userList = document.getElementById('user-list')
+    userList.innerHTML = users.map(user => template(user)).join('')
+}
+
+const addformListener = () => {
+    const userForm = document.getElementById('user-form')
+    userForm.onsubmit =  async (e) => {
+    e.preventDefault()
+    const formData = new FormData(userForm)
+    // console.log(formData.get('name'))
+    const data = Object.fromEntries(formData.entries())
+    // console.log(data)
+    await fetch('/users', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    userForm.reset()
+    getUsers()
+    }
+}
+
+window.onload = () => {
+    loadInitialTemplate()
+    addformListener()
+    getUsers()
+}
