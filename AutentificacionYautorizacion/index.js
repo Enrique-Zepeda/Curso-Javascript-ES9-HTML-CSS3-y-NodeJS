37f72f873aff83a3118054232600bf6635bc0a14
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-01-13 13:33:36 -0600

    Middleware de validacion del JWT

--- AutentificacionYautorizacion/index.js
+++ AutentificacionYautorizacion/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const expressJwt = require('express-jwt');
+const { expressjwt: expressJwt } = require('express-jwt')
 const User = require('./user');
 
 
@@ -12,7 +12,8 @@ const app = express()
 
 app.use(express.json())
 
-const singToken = _id => jwt.sign( { _id }, 'secret')
+const validateJwt = expressJwt({ secret: 'secreto', algorithms: ['HS256']})
+const singToken = _id => jwt.sign( { _id }, 'secreto')
 
 app.post('/register', async (req, res) => {
     const { body } = req
@@ -45,13 +46,48 @@ app.post('/login', async (req, res) => {
                 const signed = singToken(user._id)
                 res.status(200).send(signed)
             } else{
-                res.status(403).send('usuario t/o contraseña incorrecta')
+                res.status(403).send('usuario y/o contraseña incorrecta')
             }
         }
     }catch(err){
         console.log(err)
         res.status(500).send(err.message)
     }
+})
+
+// app.get('/lele',validateJwt, async (req, res, next) => {
+//     // console.log('lala', req.auth)
+//     try{
+//         const user = await User.findById(req.auth._id)
+//         if(!user){
+//             return res.status(401).end
+//         }
+//         req.auth = user
+//         next()
+//     }catch(err){
+//         next(e)
+//     }
+// }, (req, res) => {
+//     res.send(req.auth)
+// })
+
+const findAndAssignUser = async (req, res, next) => {
+        // console.log('lala', req.auth)
+        try{
+            const user = await User.findById(req.auth._id)
+            if(!user){
+                return res.status(401).end
+            }
+            req.auth = user
+            next()
+        }catch(err){
+            next(e)
+        }
+    }
+
+const isAuthenticated = express.Router().use(validateJwt, findAndAssignUser)
+app.get('/lele',isAuthenticated, (req, res) => {
+    res.send(req.auth)
 })
 
 app.listen(3000, () => {
