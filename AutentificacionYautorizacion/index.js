903d8c4575a823494cc3ff8087438e2920c4ffbd
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-01-14 10:51:30 -0600

    midleware para manejo de errores

--- AutentificacionYautorizacion/index.js
+++ AutentificacionYautorizacion/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const expressJwt = require('express-jwt');
+const { expressjwt: expressJwt } = require('express-jwt')
 const User = require('./user');
 
 
@@ -11,6 +11,9 @@ mongoose.connect('mongodb+srv://Zepeda:36516088@cluster0.chnghm9.mongodb.net/aut
 const app = express()
 
 app.use(express.json())
+
+const validateJwt = expressJwt({ secret: 'secreto', algorithms: ['HS256']})
+const singToken = _id => jwt.sign( { _id }, 'secreto')
 
 app.post('/register', async (req, res) => {
     const { body } = req
@@ -23,12 +26,77 @@ app.post('/register', async (req, res) => {
         const salt = await bcrypt.genSalt()
         const hashed = await bcrypt.hash(body.password, salt)
         const user = await User.create({ email: body.email, password: hashed, salt})
-
-        res.send({ _id: user._id })
+        const signed = singToken(user._id)
+        res.status(201).send(signed)
     } catch(err) {
         console.log(err)
         res.status(500).send(err.message)
     }
+})
+
+app.post('/login', async (req, res) => {
+    const { body } = req
+    try{
+        const user = await User.findOne({ email: body.email })
+        if(!user){
+            res.status(403).send('usuario t/o contraseña incorrecta')
+        }else{
+            const isMatch = await bcrypt.compare(body.password, user.password)
+            if(isMatch){
+                const signed = singToken(user._id)
+                res.status(200).send(signed)
+            } else{
+                res.status(403).send('usuario y/o contraseña incorrecta')
+            }
+        }
+    }catch(err){
+        console.log(err)
+        res.status(500).send(err.message)
+    }
+})
+
+// app.get('/lele',validateJwt, async (req, res, next) => {
+//     // console.log('lala', req.auth)
+//     try{
+//         const user = await User.findById(req.auth._id)
+//         if(!user){
+//             return res.status(401).end
+//         }
+//         req.auth = user
+//         next()
+//     }catch(err){
+//         next(e)
+//     }
+// }, (req, res) => {
+//     res.send(req.auth)
+// })
+
+const findAndAssignUser = async (req, res, next) => {
+        // console.log('lala', req.auth)
+        try{
+            const user = await User.findById(req.auth._id)
+            if(!user){
+                return res.status(401).end()
+            }
+            req.auth = user
+            next()
+        }catch(e){
+            next(e)
+        }
+    }
+
+const isAuthenticated = express.Router().use(validateJwt, findAndAssignUser)
+app.get('/lele',isAuthenticated, (req, res) => {
+    throw new Error('nuevo error')
+    // res.send(req.auth)
+})
+
+app.use((err, req, res, next) => {
+    console.error('Mi nuevo error', err.stack)
+    next(err)
+})
+app.use((err, req, res, next) => {
+    res.send('Ha ocurrido un error :(')
 })
 
 app.listen(3000, () => {
