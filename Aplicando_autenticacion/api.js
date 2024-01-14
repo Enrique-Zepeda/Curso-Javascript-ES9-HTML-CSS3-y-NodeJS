fbf7c8b58822ad5da89ab3bc73e24f5899070ecf
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-01-14 14:21:04 -0600

    login y register

--- Aplicando_autenticacion/api.js
+++ Aplicando_autenticacion/api.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const Animal = require('./animal.controller')
+const { Auth, isAuthenticated} = require('./auth.controller')
 const port = 3000
 
 mongoose.connect('mongodb+srv://Zepeda:36516088@cluster0.chnghm9.mongodb.net/animals?retryWrites=true&w=majority')
@@ -9,11 +10,14 @@ mongoose.connect('mongodb+srv://Zepeda:36516088@cluster0.chnghm9.mongodb.net/ani
 app.use(express.json())
 
 // ENDPOINTS
-app.get('/animals', Animal.list)
-app.post('/animals', Animal.create)
-app.put('/animals/:id', Animal.update)
-app.patch('/animals/:id', Animal.update)
-app.delete('/animals/:id', Animal.destroy)
+app.get('/animals', isAuthenticated, Animal.list)
+app.post('/animals', isAuthenticated, Animal.create)
+app.put('/animals/:id', isAuthenticated, Animal.update)
+app.patch('/animals/:id', isAuthenticated, Animal.update)
+app.delete('/animals/:id', isAuthenticated, Animal.destroy)
+
+app.post('/login', Auth.login)
+app.post('/register', Auth.register)
 
 app.use(express.static('app'))
 
---
+++ Aplicando_autenticacion/auth.controller.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { expressjwt: expressJwt } = require('express-jwt')
+const User = require('/user.model');
+const { model } = require('mongoose');
+
+const validateJwt = expressJwt({ secret: process.env.secret, algorithms: ['HS256']})
+
+const singToken = _id => jwt.sign({ _id }, process.env.secret)
+
+findAndAssignUser = async (req, res, next) => {
+    try{
+        const user = await User.findById(req.user._id)
+        if(!user){
+            return res.estatus(401).end()
+        }
+        req.user = user
+        next()
+    }catch(e){
+        next(e)
+    }
+}
+
+const isAuthenticated = express.Router().use(validateJwt, findAndAssignUser)
+
+const Auth = {
+    login: async (req, res) => {
+        const { body } = req
+        try{
+            const user = await User.findOne({ email: body.email})
+            if(!user){
+                res.status(401).send('usuario o contraseña invalida')
+            }else{
+                const isMatch = await bcrypt.compare(body.password, user.password)
+                if(isMatch){
+                    const signed = singToken(user._id)
+                    res.status(200).send(signed)
+                }else{
+                    res.status(401).send('usuario o contraseña invalida')
+                }
+            }
+        }catch(e){
+            res.send(e.message)
+        }
+    },
+    register: async (req,res) => {
+        const { body } = req
+        try{
+            const isUser = await User.findOne({ email: body.email})
+            if(isUser){
+                res.send('Usuario ya existe')
+            }else{
+                const salt = await bcrypt.genSalt()
+                const hashed = await bcrypt.hash(body.password, salt)
+                const user = await User.create({ email: body.email, password: hashed, salt})
+                const signed = singToken(user._id)
+                res.send(signed)
+            }
+        }catch(err){
+            res.status(500).send(err.message)
+        }
+    },
+}
+
+module.exports = { Auth, isAuthenticated }
